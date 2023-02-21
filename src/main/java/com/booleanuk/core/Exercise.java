fe4ba0bb2215007808a7424c5a98b2db09cdf67e
Lewis Campbell <ljoncb@gmail.com>
2023-02-21 15:59:47 +0000

    initial setup

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -1,7 +1,13 @@
 package com.booleanuk.core;
 
 public class Exercise {
-    public static String sayHelloWorld() {
-        return "Hello, world!";
+
+    public String[] fizzbuzz(int lower, int higher) {
+        /* TODO: Implement a function that will create a list of strings containing the numbers from the lower number to the higher number
+            Where a number is a multiple of three (3, 6, 9, etc) the array should contain the string "Fizz" instead of a number.
+            Where a number is a multiple of five (5, 10, etc) the array should contain the string "Buzz" instead of a number.
+            Where a number is a multiple of both five AND three (15, 30, etc) the array should contain the string "Fizzbuzz" instead of a number.
+        * */
+        return null;
     }
 }
--- src/test/java/com/booleanuk/core/ExerciseTest.java
+++ src/test/java/com/booleanuk/core/ExerciseTest.java
@@ -5,8 +5,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ExerciseTest {
+
+    Exercise exercise;
+    public ExerciseTest() {
+        this.exercise = new Exercise();
+    }
     @Test
-    public void shouldSayHelloWorld() {
-        Assertions.assertEquals("Hello, world!", Exercise.sayHelloWorld());
+    public void fizzbuzz() {
+        String[] shouldBeOne = {"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14"};
+        Assertions.assertArrayEquals(shouldBeOne, this.exercise.fizzbuzz(1, 15));
     }
 }
