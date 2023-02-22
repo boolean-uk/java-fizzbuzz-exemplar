54c1e4e8dffe437472169c10a6aa17f4d4d788be
john <johnny.lioup@gmail.com>
2023-02-22 16:54:47 +0000

    added examplar for core and extension

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -18,9 +18,15 @@ public class Exercise {
             Where a number is a multiple of five (5, 10, etc) the number in the array should be replaced the string "Buzz".
             Where a number is a multiple of both five AND three (15, 30, etc) the number in the array should be replaced the string "Fizzbuzz".
         * */
-
-
-        
+        for (int i = 0; i < nums.length; i++) {
+            if ((Integer.parseInt(nums[i]) % 15) == 0) {
+                nums[i] = "Fizzbuzz";
+            } else if((Integer.parseInt(nums[i]) % 3) == 0) {
+                nums[i] = "Fizz";
+            } else if((Integer.parseInt(nums[i]) % 5) == 0) {
+                nums[i] = "Buzz";
+            }
+        }
         return nums;
     }
 }
--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -1,15 +1,25 @@
 package com.booleanuk.extension;
 
 public class Extension {
+    String[] nums;
     public String[] fizzbuzz(int lower, int higher) {
         /* TODO: Implement a function that will create a list of strings containing the numbers from the lower number to the higher number
             Where a number is a multiple of three (3, 6, 9, etc) the array should contain the string "Fizz" instead of a number.
             Where a number is a multiple of five (5, 10, etc) the array should contain the string "Buzz" instead of a number.
             Where a number is a multiple of both five AND three (15, 30, etc) the array should contain the string "Fizzbuzz" instead of a number.
         * */
-
-
-
-        return null;
+        this.nums = new String[higher - lower + 1];
+        for (int i = lower, index = 0; i <= this.nums.length; i++, index++) {
+            if ((i % 15) == 0) {
+                this.nums[index] = "Fizzbuzz";
+            } else if((i % 3) == 0) {
+                this.nums[index] = "Fizz";
+            } else if((i % 5) == 0) {
+                this.nums[index] = "Buzz";
+            } else {
+                this.nums[index] = Integer.toString(i);
+            }
+        }
+        return this.nums;
     }
 }
